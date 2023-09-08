91b7e181748f8bd07f6249a234439a770672601d
Derrick Ampire <ampire90@gmail.com>
2023-09-08 15:31:00 +0300

    header component rendered

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`$inter.className bg-slate-100 text-slate-900 relative`}>
+      <body className={`$inter.className bg-slate-100 text-slate-900 relative h-[5000px]`}>
         <div className='bg-[#bfc97e] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#c4e3f1] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+
+        <Header />
         {children}
       </body>
     </html>
