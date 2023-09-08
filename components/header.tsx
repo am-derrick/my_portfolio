8a668751fd571ed6e652132e6771f1808e61f252
Derrick Ampire <ampire90@gmail.com>
2023-09-08 18:28:27 +0300

    header styled more

--- components/header.tsx
+++ components/header.tsx
@@ -15,13 +15,18 @@ export default function Header() {
             >
             </motion.div>
 
-            <nav className='fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-                <ul>
+            <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+                <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[1.3rem] font-medium text-blue-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
                     {
                         sections.map(section => (
-                            <li key={section.hash}>
-                                <Link href={section.hash}>{section.name}</Link>
-                            </li>
+                            <motion.li
+                                className='h-3/4 flex items-center justify-center relative'
+                                key={section.hash}
+                                initial={{ y: -100,opacity: 0 }}
+                                animate={{ y: 0, opacity: 1 }}
+                            >
+                                <Link  className='flex w-full items-center justify-center px-3 py-3 hover:text-blue-900 transition' href={section.hash}>{section.name}</Link>
+                            </motion.li>
                         ))
                     }
                 </ul>
