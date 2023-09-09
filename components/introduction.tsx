eb2f34f303cc118c7ae441c72a95f5d4cf8bd00b
Derrick Ampire <ampire90@gmail.com>
2023-09-09 09:45:09 +0300

    host configured

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -9,7 +9,8 @@ export default function Introduction() {
                 <Image
                     src='https://media.licdn.com/dms/image/C4D03AQGy8uJqcf9rTw/profile-displayphoto-shrink_800_800/0/1662806566762?e=1699488000&v=beta&t=H6JnYGofWCrFkkx-kFaI_u8mUQKUnTV0_sFHgXIHZ4M'
                     alt="Derrick Ampire portrait"
-                    width='192' height='192' 
+                    width='192' height='192'
+                    quality='95' priority={true}
                 />
             </div>
         </div>
