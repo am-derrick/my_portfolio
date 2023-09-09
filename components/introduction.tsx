e4edf32f9a2794b53a64c1c2fc01283c0117ea62
Derrick Ampire <ampire90@gmail.com>
2023-09-09 10:55:37 +0300

    more info added

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -1,19 +1,32 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function Introduction() {
   return (
     <section>
         <div className='flex items-center justify-center'>
-            <div>
+            <motion.div
+                whileHover={{ scale: [null, 1.5, 1.4] }}
+                transition={{ duration: 0.3 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+            >
                 <Image
                     src='https://media.licdn.com/dms/image/C4D03AQGy8uJqcf9rTw/profile-displayphoto-shrink_800_800/0/1662806566762?e=1699488000&v=beta&t=H6JnYGofWCrFkkx-kFaI_u8mUQKUnTV0_sFHgXIHZ4M'
                     alt="Derrick Ampire portrait"
                     width='192' height='192'
                     quality='95' priority={true}
+                    className='h-30 w-30 rounded-full'
                 />
-            </div>
+            </motion.div>
         </div>
+
+        <p>
+            <span>Hello I&apos;m </span><span className="font-bold">Derrick Ampire</span>, a Software Engineer with a preference for <span className="font-bold">backend and cloud development </span><span>with experience in AWS & GCP, programming in Typescript, Javascript and Python and devops. I have a strong passion for teaching and mentoring.</span>
+        </p>
     </section>
   )
 }
