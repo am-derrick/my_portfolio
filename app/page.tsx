a0d8a6e6f5fb989d53b2497553c89505075fe1e6
Derrick Ampire <ampire90@gmail.com>
2023-09-11 13:51:30 +0300

    projects page rendered

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Introduction from "@/components/introduction";
 import About from "@/components/about";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Introduction />
       <About />
+      <Projects />
     </main>
   )
 }
