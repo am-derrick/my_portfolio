e65d9143cedde00a60d2eef5295c7b0dccd9fe49
Derrick Ampire <ampire90@gmail.com>
2023-09-13 17:38:41 +0300

    section added

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SectionHeading from './section-heading';
 import { myProjects } from '@/lib/data';
+import Image from 'next/image';
 
 export default function Projects() {
   return (
@@ -23,6 +24,15 @@ type ProjectProps = (typeof myProjects)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
     return (
-        <div>{title}</div>
+        <section className='bg-white max-w-[42rem] border border-blue-950 overflow-hidden sm:pr-8'>
+            <h3 className='text-2xl font-semibold'>{title}</h3>
+            <p className='mt-2 leading-relaxed text-blue-950'>{description}</p>
+            <ul>
+                {tags.map((tag, index) =>(
+                    <li className='bg-blue-500 px-3 py-1 text-[0.7rem] tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                ))}
+            </ul>
+            <Image src={imageUrl} alt="My projects" quality={95} />
+        </section>
     )
 }
