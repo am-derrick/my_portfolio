1f3fbb8e4409168cdaefc9dfd053d85be0dfec45
Derrick Ampire <ampire90@gmail.com>
2023-09-14 12:50:45 +0300

    project section styled further

--- components/projects.tsx
+++ components/projects.tsx
@@ -24,15 +24,19 @@ type ProjectProps = (typeof myProjects)[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
     return (
-        <section className='bg-white max-w-[42rem] border border-blue-950 overflow-hidden sm:pr-8'>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
-            <p className='mt-2 leading-relaxed text-blue-950'>{description}</p>
-            <ul>
+        <section className='bg-zinc-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last:mb-0'>
+
+            <div className='pt-3 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+                <h3 className='text-2xl font-semibold'>{title}</h3>
+                <p className='mt-2 leading-relaxed text-blue-950'>{description}</p>
+                <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                 {tags.map((tag, index) =>(
-                    <li className='bg-blue-500 px-3 py-1 text-[0.7rem] tracking-wider text-white rounded-full' key={index}>{tag}</li>
-                ))}
-            </ul>
-            <Image src={imageUrl} alt="My projects" quality={95} />
+                    <li className='bg-sky-950 px-3 py-1 text-[0.7rem] tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                ) )}
+                </ul>
+            </div>
+            
+            <Image src={imageUrl} alt="My projects" quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl' />
         </section>
     )
 }
